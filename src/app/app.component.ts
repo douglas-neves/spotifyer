1b3636bea3d189ee6b76e3c62437f0900b20a426
Douglas Neves <dsouza@tray.net.br>
2020-03-06 17:26:40 -0300

    #1 - Configuração inicial | Autenticação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/Auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'spotifyer';
+  public title = 'spotifyer';
+
+  constructor(public Auth: AuthService) {
+    // handle this
+  }
+
+  /**
+   *
+   */
+  login() {
+    const user = this.Auth.login();
+
+    console.log(user);
+  };
 }
